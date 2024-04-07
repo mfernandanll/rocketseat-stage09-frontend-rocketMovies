96f09b74d4a83f28e9c4c5b066126c6e64b4efbd
Fernanda Loureiro <m.fernanda.nll@gmail.com>
2024-04-07 19:11:46 -0400

    Creating theme.js and global.js

---
+++ src/styles/theme.js
@@ -0,0 +1,10 @@
+export default {
+  COLORS: {
+    WHITE: "#F4EDE8",
+    GRAY_100: "#948F99",
+    GRAY_200: "#999591",
+    GRAY_300: "#666360",
+    GRAY_400: "#312E38",
+    ROSE: "#FF859B",
+  }
+}
