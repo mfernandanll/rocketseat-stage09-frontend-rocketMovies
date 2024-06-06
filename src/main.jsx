ea74ca682ba846027ae81c7475dc80649f516d33
Fernanda Loureiro <m.fernanda.nll@gmail.com>
2024-06-06 13:15:31 -0400

    Adding routes to the project

--- src/main.jsx
+++ src/main.jsx
@@ -5,18 +5,13 @@ import GlobalStyles from './styles/global'
 
 import theme from './styles/theme'
 
-import { Home } from './pages/Home'
-import { CreateMovie } from './pages/CreateMovie'
-import { MoviePreview } from './pages/MoviePreview'
-import { Profile } from './pages/Profile'
-import { SignIn } from './pages/SignIn'
-import { SignUp } from './pages/SignUp'
+import { Routes } from './routes'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <SignUp />
+      <Routes />
     </ThemeProvider>
   </React.StrictMode>,
 )
---
+++ src/routes/app.routes.jsx
@@ -0,0 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+
+import { CreateMovie } from "../pages/CreateMovie";
+import { MoviePreview } from "../pages/MoviePreview";
+
+import { Home } from "../pages/Home";
+import { Profile } from "../pages/Profile";
+
+export function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/createMovie" element={<CreateMovie />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/moviePreview/:id" element={<MoviePreview />} />
+    </Routes>
+  );
+}
