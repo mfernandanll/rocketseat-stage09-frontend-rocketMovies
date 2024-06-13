37f6893851f40d27e8cad10815dde3b472382721
Fernanda Loureiro <m.fernanda.nll@gmail.com>
2024-06-13 16:35:59 -0400

    Setting user register

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,11 +1,42 @@
+import { useState } from "react";
 import { FiLock, FiMail, FiUser } from "react-icons/fi";
+import { FaArrowLeft } from "react-icons/fa6";
+import { Link, useNavigate } from "react-router-dom";
+
+import { api } from "../../services/api";
+
 import { InputField } from "../../components/InputField";
 import { Button } from "../../components/Button";
+
 import { Background, Container, Form } from "./styles";
-import { FaArrowLeft } from "react-icons/fa6";
-import { Link } from "react-router-dom";
 
 export function SignUp() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  function handleSignUp() {
+    if (!name || !email || !password) {
+      return alert("Preencha todos os campos!");
+    }
+
+    api
+      .post("/users", { name, email, password })
+      .then(() => {
+        alert("Cadastro realizado com sucesso!");
+        navigate("/");
+      })
+      .catch((error) => {
+        if (error.response) {
+          alert(error.response.data.message);
+        } else {
+          alert("Não foi possível cadastrar.");
+        }
+      });
+  }
+
   return (
     <Container>
       <Form>
@@ -13,11 +44,27 @@ export function SignUp() {
         <p>Aplicação para acompanhar tudo que assistir.</p>
         <h2>Crie sua conta</h2>
 
-        <InputField placeholder="Nome" type="text" icon={FiUser} />
-        <InputField placeholder="E-mail" type="text" icon={FiMail} />
-        <InputField placeholder="Senha" type="password" icon={FiLock} />
+        <InputField 
+          placeholder="Nome" 
+          type="text" 
+          icon={FiUser} 
+          onChange={e => setName(e.target.value)}
+        />
+        <InputField 
+          placeholder="E-mail" 
+          type="text" 
+          icon={FiMail} 
+          onChange={e => setEmail(e.target.value)}
+        />
+        <InputField 
+          placeholder="Senha" 
+          type="password" 
+          icon={FiLock} 
+          onChange={e => setPassword(e.target.value)}
+        />
 
-        <Button title="Cadastrar" />
+        <Button title="Cadastrar" onClick={handleSignUp}/>
+        
         <Link to="/">
           <FaArrowLeft /> Voltar para o login
         </Link>
