6ee2262b8bf72a34747b11fbae769e8e74b715ed
Fernanda Loureiro <m.fernanda.nll@gmail.com>
2024-06-14 10:35:53 -0400

    Integrate sign in with backend api

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,17 +1,39 @@
 import {
   createContext,
   useContext,
+  useState
 } from 'react';
 
 const AuthContext = createContext({});
 
+import { api } from "../services/api";
 
 function AuthProvider({ children }) {
+  const [data, setData] = useState({});
+
+  async function signIn({ email, password }) {
+    try {
+      const response = await api.post("sessions", { email, password });
+      const { token, user } = response.data;
+      console.log(response)
+      api.defaults.headers.authorization = `Bearer ${token}`;
+
+      setData({ token, user });
+
+    } catch (error) {
+      if (error.response) {
+        alert(error.response.data.message);
+      } else {
+        alert("Não foi possível entrar.");
+      }
+    }
+  }
+
 
   return (
     <AuthContext.Provider value={{
-      name: "maria",
-      email: "maria@example"
+      signIn,
+      user: data.user
     }}>
       {children}
     </AuthContext.Provider>
--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -5,11 +5,18 @@ import { Background, Container, Form } from "./styles";
 import { Link } from "react-router-dom";
 
 import { useAuth } from '../../hooks/auth';
+import { useState } from "react";
 
 export function SignIn() {
 
-  const data = useAuth();
-  console.log(data);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { signIn } = useAuth();
+
+  function handleSignIn() {
+    signIn({ email, password });
+  }
 
   return (
     <Container>
@@ -18,11 +25,21 @@ export function SignIn() {
         <p>Aplicação para acompanhar tudo que assistir.</p>
         <h2>Faça seu login</h2>
 
-        <InputField placeholder="E-mail" type="text" icon={FiMail} />
+        <InputField 
+          placeholder="E-mail" 
+          type="text" 
+          icon={FiMail} 
+          onChange={e => setEmail(e.target.value)}
+        />
 
-        <InputField placeholder="Senha atual" type="password" icon={FiLock} />
+        <InputField 
+          placeholder="Senha atual" 
+          type="password" 
+          icon={FiLock} 
+          onChange={e => setPassword(e.target.value)}
+        />
 
-        <Button title="Entrar" />
+        <Button title="Entrar" onClick={handleSignIn} />
         <Link to="register" >Criar Conta</Link>
       </Form>
       <Background/>
