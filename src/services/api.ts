dfde5a7be3983c5947e23a19774f1a698caed8b2
Fernanda Loureiro <m.fernanda.nll@gmail.com>
2024-08-15 18:53:46 -0400

    Changing api baseurl

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  // baseURL: 'https://movie-notes-api-ajo8.onrender.com'
-  baseURL: 'http://localhost:3333'
+  baseURL: 'https://movie-notes-api-ajo8.onrender.com'
 });
